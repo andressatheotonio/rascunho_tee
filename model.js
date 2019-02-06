dd34307e93535089b8ddd9df304d8ef1b101f086
Andressa Theotônio <43258476+andressatheotonio@users.noreply.github.com>
2019-02-06 14:22:13 -0300

    Update model.js

--- model.js
+++ model.js
@@ -11,22 +11,22 @@ var Loader = function () {
         }
       };
 
-	return {
-  	init: function (options) {
+  return {
+    init: function (options) {
       options = options || {};
       var time = options.time ? options.time : 0,
-	        interval = time/100;
-      
-    	loader.classList.add('run');
+          interval = time/100;
+
+      loader.classList.add('run');
       k = window.setInterval(counter, interval); 
       setTimeout(function () {        
-      	loader.classList.add('done');
+        loader.classList.add('done');
       }, time);
     },
   }
 }();
 
 Loader.init({
-  	// If you have changed the @time in LESS, update this number to the corresponding value. Measured in miliseconds.
-  	time: 10000
+  // If you have changed the @time in LESS, update this number to the corresponding value. Measured in miliseconds.
+  time: 10000
 });
